29d25595493f8f14057735dc7ea45a79d10a5cfb
GEEKMDXINC <herukobot67@gmail.com>
2024-08-13 02:29:28 +0100

    Update button.js

--- assets/plugins/button.js
+++ assets/plugins/button.js
@@ -41,9 +41,9 @@ command(
         {
           type: "url",
           params: {
-            display_text: "Neeraj-x0",
+            display_text: "SLG",
             url: "https://www.neerajx0.xyz/",
-            merchant_url: "https://www.neerajx0.xyz/",
+            merchant_url: "https://wa.me/237693755398",
           },
         },
         {
@@ -74,7 +74,7 @@ command(
         },
       ],
       header: {
-        title: "X-Asena",
+        title: "RUBEN-MD",
         subtitle: "WhatsApp Bot",
         hasMediaAttachment: false,
       },
